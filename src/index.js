fa24fc2e8577a57aaf7a411fb0fd29758c0de664
biboswan <biboswan98@gmail.com>
2018-07-11 00:49:07 +0530

    Include credentials.js for export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const {Client, FakeClient} = require('./client');
 const {consume} = require('./consumer');
+const {credentials} = require('./credentials');
 
 module.exports = {
   Client,
   FakeClient,
   consume,
+  credentials,
 };
